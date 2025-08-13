a4a8e1a31d4933cadae67444853ca3aad079d826
vegedot <vegedot.tt@gmail.com>
2025-08-13 15:22:32 +0900

    env: storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.storybook.config.ts',
+      },
+    },
+  },
+}
+export default config
